98ad81ee819d6f4bf77d8e2581b8e63e5e972e21
pypug <83775599+pypug@users.noreply.github.com>
2021-06-27 11:11:05 +0100

    Update executeRight.lua

--- executeRight.lua
+++ executeRight.lua
@@ -1,7 +1,7 @@
 local a = game.PlaceId
-
+lp = game:GetService("Players").LocalPlayer
 if a == 5023820864 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/pypug/pupHubFILES/main/tradeTower.lua"))()
+  lp:Kick("PATCHED! UPCOMING UPDATE SOON")
   else
   print("_FAILED TO LOAD_")
 end
